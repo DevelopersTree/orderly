cb1a4fa2ecb3978d2ab01fbe3227aae602b0c5c7
aramrafeq <aramrafeq2@gmail.com>
2019-11-02 14:49:55 +0300

    electron basic app created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-const watcher = require('./watcher');
+// const watcher = require('./watcher');
 
-const dirToWatch = '/Users/hozan/Desktop/watched';
-watcher.lunch(dirToWatch);
+// const dirToWatch = '/Users/hozan/Desktop/watched';
+// watcher.lunch(dirToWatch);
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  win.loadFile('./ui/index.html');
+}
+
+app.on('ready', createWindow);
